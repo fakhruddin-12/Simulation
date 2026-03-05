d2e7414c0702460b397ba770b7a20dc90c71f9b4
fakhruddin-12 <s2845938@ed.ac.uk>
2026-03-05 00:55:28 +0000

    Update Rider.py

--- Classes/Rider.py
+++ Classes/Rider.py
@@ -1,12 +1,19 @@
+import random
+
 class Rider:
     
-    def __init__(self , rider_id , request_time , pickup_location , dropoff_location):
+    def __init__(self , rider_id , request_time , pickup_location , dropoff_location , patience_lambda):
         self.rider_id = rider_id
         self.request_time = request_time
         self.pickup_location = pickup_location
         self.dropoff_location = dropoff_location
+        self.patience_lambda = patience_lambda
+
+        self.patience = random.expovariate(self.patience_lambda)
+        self.deadline = self.request_time + self.patience
 
         self.matched = False
         self.driver_id = None
         self.pickup_time = None
         self.dropoff_time = None
+
