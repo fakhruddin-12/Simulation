a4e18f823d24ece21841425d3e052dfa4528feaa
fakhruddin-12 <s2845938@ed.ac.uk>
2026-03-05 00:53:59 +0000

    Update Driver.py

--- Classes/Driver.py
+++ Classes/Driver.py
@@ -1,8 +1,13 @@
+import random
+
 class Driver:
     
-    def __init__(self , driver_id , location , available = True , available_time = 0.0):
+    def __init__(self , driver_id , location , entry_time , offline_start , offline_end):
         self.driver_id = driver_id
         self.location = location
-        self.available = available
-        self.available_time = available_time
-        self.current_trip_id = None
+        self.entry_time = entry_time
+        self.offline_start = offline_start
+        self.offline_end = offline_end
+        
+        self.available = True
+        self.offline_time = self.entry_time + random.uniform(self.offline_start , self.offline_end)
