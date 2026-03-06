7c9915cd59bfd4c949701b0ee76a80d71929d1e3
mansineb <s2861549@ed.ac.uk>
2026-03-06 20:31:32 +0000

    Update Driver to suit other code.py

--- classes/Driver.py
+++ classes/Driver.py
@@ -2,12 +2,19 @@ import random
 
 class Driver:
     
-    def __init__(self , driver_id , location , entry_time , offline_start , offline_end):
+    def __init__(self , driver_id , location , entry_time , offline_time):
         self.driver_id = driver_id
         self.location = location
         self.entry_time = entry_time
-        self.offline_start = offline_start
-        self.offline_end = offline_end
+        
+        #self.offline_start = offline_start  #old code
+        #self.offline_end = offline_end    #old code
+        
+        self.offline_time = offline_time   # time driver leaves system, changed by MANSI
         
         self.available = True
-        self.offline_time = self.entry_time + random.uniform(self.offline_start , self.offline_end)
+        #self.offline_time = self.entry_time + random.uniform(self.offline_start , self.offline_end)    #old code
+
+        # driver availability status
+        self.available = True
+
