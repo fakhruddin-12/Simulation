bd0baae099b64530edaa74cee30cb041cdaccdf3
mansineb <s2861549@ed.ac.uk>
2026-03-06 23:17:59 +0000

    Update generate_random.py

--- classes/generate_random.py
+++ classes/generate_random.py
@@ -1,13 +1,10 @@
 import random
 import math
 
-
 class Distributions:
 
     def __init__(self, simulation):
-
         self.simulation = simulation
-
         # setting model parameters
         self.rider_arrival_rate = 30
         self.driver_arrival_rate = 3
@@ -17,50 +14,33 @@ class Distributions:
 
     # Rider arrival process
     def rider_arrival(self):
-
         return random.expovariate(self.rider_arrival_rate)
-
 
     # Driver arrival process
     def driver_arrival(self):
-
         return random.expovariate(self.driver_arrival_rate)
-
 
     # Rider patience
     def rider_patience(self):
-
         return random.expovariate(self.patience_rate)
-
 
     # Driver availability duration
     def driver_availability(self):
+        return random.uniform(self.driver_availability_min, self.driver_availability_max)
 
-        return random.uniform(
-            self.driver_availability_min,
-            self.driver_availability_max
-        )
-
-
-    # Generate random location in Squareshire
-    # City is 20 x 20 miles
+    # Generate random location in Squareshire 20 X 20 miles
     def random_location(self):
-
         x = random.uniform(0, 20)
         y = random.uniform(0, 20)
-
         return (x, y)
-
-
+    
+    # Expected travel time basis average speed
+    def expected_travel_time(self, loc1, loc2):
+        # Euclidean distance
+        d = math.sqrt( (loc1[0] - loc2[0])**2 + (loc1[1] - loc2[1])**2)
+        return d / 20
+    
     # Actual trip time distribution
     def travel_time(self, loc1, loc2):
-
-        # Euclidean distance
-        d = math.sqrt(
-            (loc1[0] - loc2[0])**2 +
-            (loc1[1] - loc2[1])**2
-        )
-
-        mu = d / 20
-
-        return random.uniform(0.8 * mu, 1.2 * mu)
+        mu = self.expected_travel_time(loc1, loc2)
+        return random.uniform(0.8*mu, 1.2*mu)
