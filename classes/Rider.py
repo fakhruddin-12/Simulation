6a7323915cc6ebab3adedbbe1e7ebb6ecd78e21c
fakhruddin-12 <s2845938@ed.ac.uk>
2026-03-09 01:21:49 +0000

    Update Rider.py

--- classes/Rider.py
+++ classes/Rider.py
@@ -1,20 +1,50 @@
 import random
 
-class Rider:
+#class Rider:
     
-    def __init__(self , rider_id , request_time , pickup_location , dropoff_location , patience):
+    #def __init__(self , rider_id , request_time , pickup_location , dropoff_location , patience):
+        #self.rider_id = rider_id
+        #self.request_time = request_time
+        #self.pickup_location = pickup_location
+        #self.dropoff_location = dropoff_location
+        #self.patience_lambda = patience_lambda
+        #self.patience = patience      #changed by MANSI
+        
+        #self.patience = random.expovariate(self.patience_lambda)
+        #self.deadline = self.request_time + self.patience
+        #self.deadline = request_time + patience        #changed by MANSI
+        
+        #self.matched = False
+        #self.driver_id = None
+        #self.pickup_time = None
+        #self.dropoff_time = None
+
+# ===== NEW CODE =====
+
+class Rider:
+
+    def __init__(self, rider_id, request_time, pickup_location, dropoff_location, patience):
+
         self.rider_id = rider_id
         self.request_time = request_time
         self.pickup_location = pickup_location
         self.dropoff_location = dropoff_location
-        #self.patience_lambda = patience_lambda
-        self.patience = patience      #changed by MANSI
-        
-        #self.patience = random.expovariate(self.patience_lambda)
-        #self.deadline = self.request_time + self.patience
-        self.deadline = request_time + patience        #changed by MANSI
-        
-        self.matched = False
+
+        self.patience = patience
+        self.deadline = request_time + patience
+
+        # rider state
+        # waiting, matched, in_trip, completed, abandoned
+        self.status = "waiting"
+
         self.driver_id = None
+
+        # event times
+        self.match_time = None
         self.pickup_time = None
         self.dropoff_time = None
+
+        # rider metrics
+        self.wait_time = None
+        self.trip_time = None
+        self.system_time = None
