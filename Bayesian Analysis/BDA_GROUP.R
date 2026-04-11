24ea7b17c7155c1c342928b3b34876ceaf75fe74
didarbrar <168735387+didarbrar@users.noreply.github.com>
2026-04-11 13:49:06 +0100

    update

--- Bayesian Analysis/BDA_GROUP.R
+++ Bayesian Analysis/BDA_GROUP.R
@@ -2,11 +2,12 @@
 # extracting data into R
 install.packages("readxl")
 library(readxl)
-df_stop<-read_excel("sqf-2025.xlsx")
+#df_stop<-read_excel("sqf-2025.xlsx")
+df_stop <- read_excel("/Users/didar/Documents/BDA_GROUP/Simulation/Bayesian Analysis/sqf-2025.xlsx")
 df_stop
 library(brms)
 library(dplyr)
-options(mc.cores=parllel::detectCores())
+#options(mc.cores=parllel::detectCores())
 
 df_stop[df_stop == "(null)"] <- NA
 df_stop_clean <- df_stop[
@@ -24,15 +25,45 @@ df_stop_clean <- df_stop[
 
 df_stop_clean
 
+#-------------------------------
+#as we are doing this for one quarter
+#--------------------------------
 
 df_stop_q1 <- df_stop_clean[df_stop_clean$MONTH2 %in% c("January","February","March"), ]
 
+sum(df_stop_q1$SUSPECT_REPORTED_AGE<=5)
+
+df_stop_q1<-df_stop_q1[df_stop_q1$SUSPECT_REPORTED_AGE>=5,]
+
 df_stop_q1
+
+#-------------------------------------
+#add bucket to some of covariates
+#----------------------------------
+
+# age ---------------------
+
+df_stop_q1 <- df_stop_q1 %>%
+  mutate(
+    AGE_GROUP = case_when(
+      SUSPECT_REPORTED_AGE <= 24 ~ "Young",
+      SUSPECT_REPORTED_AGE >= 25 & SUSPECT_REPORTED_AGE <= 59 ~ "Adult",
+      SUSPECT_REPORTED_AGE >= 60 ~ "Old"
+    )
+  )
+table(df_stop_q1$AGE_GROUP)
+
+df_stop_q1$AGE_GROUP<-as.factor(df_stop_q1$AGE_GROUP)
+
+#---------------------------
+# time of day
+#--------------------------
+
 
 
 model_1 <- brm(
   SUSPECT_ARRESTED_FLAG ~ SUSPECT_RACE_DESCRIPTION+
-    SUSPECT_SEX ,
+  SUSPECT_SEX + STOP_DURATION_MINUTES+AGE_GROUP ,
   data = df_stop_q1,
   family = bernoulli(),
   chains = 1,
@@ -43,5 +74,6 @@ model_1 <- brm(
 )
 
 summary(model_1)
-exp(-1.20)
-#--print
+exp(0.01)
+
+
